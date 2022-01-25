3ab3e8ba12f8fe1402752c0a6578bd89b0f40235
VictoryJu <kob0805@naver.com>
2022-01-25 20:59:56 +0900

    nodejs middleware

--- nest-tutorials/dist/app.js
+++ nest-tutorials/dist/app.js
@@ -1,12 +1,33 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
+var app_model_1 = require("./app.model");
 var app = express();
 var port = 8000;
+var data = [1, 2, 3, 4];
+app.use(function (req, res, next) {
+    console.log(req.rawHeaders[1]);
+    console.log("this is Mid");
+    next();
+});
+app.get('/cats/som', function (req, res, next) {
+    console.log("this is Som Mid");
+    next();
+});
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.send({ cats: app_model_1.Cat });
+});
+app.get('/cats/blue', function (req, res, next) {
+    res.send({ blue: app_model_1.Cat[0] });
+});
+app.get('/cats/som', function (req, res) {
+    res.send({ som: app_model_1.Cat[1] });
+});
+app.use(function (req, res, next) {
+    console.log("this is Error Mid");
+    res.send({ error: "404 not found error" });
 });
 app.listen(port, function () {
-    console.log("Example app listening on port " + port);
+    console.log("server is on " + port);
 });
 //# sourceMappingURL=app.js.map
--- nest-tutorials/src/app.ts
+++ nest-tutorials/src/app.ts
@@ -1,13 +1,40 @@
 import * as express from 'express';
+import { Cat, CatType} from './app.model';
 
 const app:express.Express = express();
 
 const port:number = 8000;
 
+const data = [1,2,3,4];
+
+app.use((req,res,next)=>{
+  console.log(req.rawHeaders[1]);
+  console.log("this is Mid");
+  next();
+});
+
+app.get('/cats/som',(req,res,next)=>{
+  console.log("this is Som Mid");
+  next();
+});
+
 app.get('/', (req:express.Request, res:express.Response) => {
-  res.send('Hello World!')
-})
+  res.send({cats: Cat});
+});
+
+app.get('/cats/blue',(req,res, next: express.NextFunction)=>{
+  res.send({blue: Cat[0]});
+});
+
+app.get('/cats/som',(req,res)=>{
+  res.send({som: Cat[1]});
+});
+
+app.use((req,res,next)=>{
+  console.log("this is Error Mid");
+  res.send({error: "404 not found error"});
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`server is on ${port}`)
 })
