613278455735d9f3ef79d7eecbad7fa13626d9f1
VictoryJu <kob0805@naver.com>
2022-01-25 21:37:54 +0900

    Express router 분리

--- nest-tutorials/dist/app.js
+++ nest-tutorials/dist/app.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
-var app_model_1 = require("./app.model");
+var cats_route_1 = require("./cats/cats.route");
 var app = express();
 var port = 8000;
 var data = [1, 2, 3, 4];
@@ -11,56 +11,7 @@ app.use(function (req, res, next) {
     next();
 });
 app.use(express.json());
-app.post('/cat', function (req, res) {
-    try {
-        var data_1 = req.body;
-        console.log(data_1);
-        app_model_1.Cat.push(data_1);
-        res.status(200).send({
-            success: true,
-            data: { data: data_1 }
-        });
-    }
-    catch (e) {
-    }
-});
-app.get('/cats', function (req, res) {
-    try {
-        var cats = app_model_1.Cat;
-        res.status(200).send({
-            success: true,
-            data: {
-                cats: cats,
-            },
-        });
-    }
-    catch (error) {
-        res.status(400).send({
-            success: false,
-            error: error.message,
-        });
-    }
-});
-app.get('/cats/:id', function (req, res) {
-    try {
-        var id_1 = req.params.id;
-        var cat = app_model_1.Cat.find(function (cat) {
-            return cat.id === id_1;
-        });
-        res.status(200).send({
-            success: true,
-            data: {
-                cat: cat,
-            },
-        });
-    }
-    catch (error) {
-        res.status(400).send({
-            success: false,
-            error: error.message,
-        });
-    }
-});
+app.use(cats_route_1.default);
 app.use(function (req, res, next) {
     console.log("this is Error Mid");
     res.send({ error: "404 not found error" });
--- nest-tutorials/src/app.ts
+++ nest-tutorials/src/app.ts
@@ -1,5 +1,5 @@
 import * as express from 'express';
-import { Cat, CatType} from './app.model';
+import catsRouter from './cats/cats.route';
 
 const app:express.Express = express();
 
@@ -16,62 +16,7 @@ app.use((req,res,next)=>{
 
 //* json middleware
 app.use(express.json());
-
-//* CREATE 새로운 고양이 추가
-app.post('/cat',(req,res)=>{
-  try{
-    const data = req.body;
-    console.log(data);
-    Cat.push(data);
-    res.status(200).send({
-      success: true,
-      data: {data}
-    })
-  }catch(e){
-
-  }
-})
-
-//* READ 고양이 전체 데이터 다 조회
-app.get('/cats',(req,res)=>{
-  try{
-    const cats = Cat;
-    res.status(200).send({
-      success: true,
-      data:{
-        cats,
-      },
-    });
-  }catch(error:any){
-    res.status(400).send({
-      success: false,
-      error: error.message,
-    });
-  }
-});
-
-//* READ 특정 고양이 데이터 조회
-app.get('/cats/:id',(req,res)=>{
-  try{
-    const id = req.params.id
-    const cat = Cat.find((cat)=>{
-      return cat.id === id
-    });
-    res.status(200).send({
-      success: true,
-      data:{
-        cat,
-      },
-    });
-  }catch(error:any){
-    res.status(400).send({
-      success: false,
-      error: error.message,
-    });
-  }
-});
-
-
+app.use(catsRouter);
 
 //* 404 middleware
 app.use((req,res,next)=>{
